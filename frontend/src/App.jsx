fb73a65c227363ae3fc541615cfd26b5af5e5f0b
DaX-523 <dakshdhama0502@gmail.com>
2024-10-18 01:17:50 +0530

    footer and about us

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,17 @@ import Button from "./components/Button";
 import Navbar from "./components/Navbar";
 import Accord from "./components/Accord";
 import Form from "./components/Form";
+import Footer from "./components/Footer";
+import AboutUs from "./components/AboutUs";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Button />
+      <AboutUs />
       <Accord />
       <Form />
+      <Footer />
     </>
   );
 }

4543d2bf74b287a0635a0f58618d96fd7a56b625
Kshitij Dhama <kshitijdhama513@gmail.com>
2024-12-01 23:42:26 +0530

    fix bubble

--- frontend/src/components/Advertiser.jsx
+++ frontend/src/components/Advertiser.jsx
@@ -6,7 +6,7 @@ const Advertiser = () => {
     <div className='w-full relative'>
       <img src="/assets/DigiGrove Website Frame 70.png" alt="" className='md:block hidden'/>
       <div
-      className={`flex flex-col align-center justify-center pt-[16px] text-[#232233] font-poppins text-center text-[150%] font-extrabold md:block hidden bg-[rgb(140,197,64)] h-[25%] w-[14.2%] absolute top-[44%] right-0 rounded-full animate-${isHovered ? 'pop' : 'pop-reversed'}`}>
+      className={`flex flex-col align-center justify-center pt-[16px] text-[#232233] font-poppins text-center text-[150%] font-extrabold bg-[rgb(140,197,64)] h-[25%] w-[14.2%] absolute top-[44%] right-0 rounded-full animate-${isHovered ? 'pop' : 'pop-reversed'}`}>
       We saved you a spot!
       </div>
 

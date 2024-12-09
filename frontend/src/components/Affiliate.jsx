d6081ca2f57f02c329f26c2ef6e810cca7b339e7
DaX-523 <dakshdhama0502@gmail.com>
2024-12-09 18:54:06 +0530

    rewrites

--- frontend/src/components/Affiliate.jsx
+++ frontend/src/components/Affiliate.jsx
@@ -1,16 +1,15 @@
 import { useNavigate } from "react-router-dom";
 
-
 const Affiliate = () => {
   const navigate = useNavigate();
 
   return (
     <div className="mt-[10%] w-full py-10">
-      <div className="text-center lg:px-20 lg:mx-40 sm:mt-10 md:mt-5">
-        <h1 className="xl:text-[128px] whitespace-nowrap  font-thunder leading-4 tracking-[0.06rem] text-[#8CC540] font-bold mb-16">
+      <div className="text-center md:ml-32 md:mr-28">
+        <h1 className="xl:text-[128px] sm:text-8xl text-6xl whitespace-nowrap  font-thunder lg:leading-4 tracking-[0.06rem] text-[#8CC540] font-bold lg:mb-12 mb-4">
           AFFILIATE MARKETING
         </h1>
-        <p className="font-poppins font-medium md:text-[18px] text-xl font-medium text-[#232233] md:mx-20 mx-10">
+        <p className="font-poppins  md:text-[18px] text-xl font-medium text-[#232233] lg:mx-20 md:mx-16 mx-10">
           We partner with third-party publishers to promote your products and
           services, ensuring you only pay for actual sales or leads. Our
           advanced analytics and high-quality affiliates maximize your reach and
@@ -25,7 +24,7 @@ const Affiliate = () => {
           <img
             src="/assets/file(3) 1.png"
             alt="paw"
-            className="overflow-hidden absolute -top-80 md:-top-[440px] lg:-left-80 -left-96 xl:-left-[374px]"
+            className="overflow-hidden absolute md:-top-80 -top-64  -left-80 xl:-left-[374px]"
             height={800}
             width={800}
           />
@@ -40,67 +39,61 @@ const Affiliate = () => {
             id="serve"
             className="w-full stroke-white stroke-1 top-0 flex gap-16 flex-wrap justify-evenly px-12"
           >
-
-            <div className="flex flex-col justify-center items-center">
-
-            <div className="mb-[-46px] text-[#8CC540] md:p-4 p-3 -mt-8 md:-mt-10 md:h-[90px] md:w-2/3 z-30 h-[60px] w-1/2 shadow-[0px_5px_5px_rgba(0,0,0,0.35)]  drop-shadow-xl bg-white/10 border border-white/20 rounded-3xl backdrop-blur-3xl">
+            <div className="flex flex-col justify-center items-center ">
+              <div className="mb-[-46px] text-[#8CC540] md:p-4 p-3 -mt-8 md:-mt-10 md:h-[90px] md:w-2/3 z-30 h-[60px] w-1/2 shadow-[0px_5px_5px_rgba(0,0,0,0.35)]  drop-shadow-xl bg-white/10 border border-white/20 rounded-3xl backdrop-blur-3xl">
                 <h1 className="text-center text-2xl md:text-4xl md:pt-1.5 font-bold">
                   Publishers
                 </h1>
               </div>
 
-
-                <div className="glass relative text-white flex flex-col items-center gap-4 md:h-[520px] md:w-[600px] h-[443px] w-[404px]  bg-white/10 border border-white/20 rounded-[35px]">
+              <div className="glass relative text-white flex flex-col items-center gap-4 md:h-[520px] md:w-[600px] sm:h-[443px] sm:w-[404px]  bg-white/10 border border-white/20 rounded-[35px]">
                 <img
                   src="/assets/Group 36.png"
                   alt="ellipses"
                   className="absolute z-10"
                 ></img>
-                <div className="pt-[80px] text-center pt-8 px-5 md:px-10 flex flex-col md:gap-10 gap-5 font-sans justify-center items-center space-y-10">
+                <div className=" text-center pt-8  sm:px-10 flex flex-col md:gap-8  font-sans justify-center items-center space-y-10">
                   <h3 className="font-bold text-2xl md:text-3xl md:px-14 px-8">
                     We assist you in maximising profits!
                   </h3>
                   <p className="font-normal text-base md:text-lg">
                     Gain substantial commissions for any leads, installations,
-                    purchases, or other transactions made by customers you brought
-                    to Digigrove via the company&apos;s website, blog posts,
-                    articles, banner ads, emails, landing pages, and social media
-                    channels.
+                    purchases, or other transactions made by customers you
+                    brought to Digigrove via the company&apos;s website, blog
+                    posts, articles, banner ads, emails, landing pages, and
+                    social media channels.
                   </p>
                 </div>
               </div>
-
-
             </div>
 
-
-            <div className="flex flex-col justify-center items-center">
-            <div className="mb-[-46px] text-[#8CC540] md:p-4 p-3 -mt-8 md:-mt-10 md:h-[90px] md:w-2/3 z-30 h-[60px] w-1/2 shadow-[0px_5px_5px_rgba(0,0,0,0.35)]  drop-shadow-xl bg-white/10 border border-white/20 rounded-3xl backdrop-blur-3xl">
+            <div className="flex flex-col justify-center items-center ">
+              <div className="mb-[-46px] text-[#8CC540] md:p-4 p-3 -mt-8 md:-mt-10 md:h-[90px] md:w-2/3 z-30 h-[60px] w-1/2 shadow-[0px_5px_5px_rgba(0,0,0,0.35)]  drop-shadow-xl bg-white/10 border border-white/20 rounded-3xl backdrop-blur-3xl">
                 <h1 className="text-center text-2xl md:text-4xl md:pt-1.5 font-bold">
                   Advertisers
                 </h1>
               </div>
 
-              <div className="glass text-white relative flex flex-col z-20 items-center gap-4  md:h-[520px] md:w-[600px] h-[443px] w-[404px]  bg-white/10 border border-white/20 rounded-[35px] ">
-              <img
-                src="/assets/Group 36.png"
-                alt="ellipses"
-                className="absolute z-10"
-              ></img>
-              <div className="pt-[80px] text-center pt-8 px-5 md:px-10 flex flex-col md:gap-10 gap-5 font-sans justify-center items-center space-y-10">
-                <h3 className=" font-bold text-2xl md:text-3xl md:px-14 px-8">
-                  Increase the global reach of your business!
-                </h3>
-                <p className="font-normal text-base md:text-lg">
-                  With our cutting-edge platform, Digigrove provide unparalleled
-                  digital marketing services to facilitate connections between
-                  brands and audiences that generate noteworthy results. We
-                  specialize in both direct response and branding strategies.
-                </p>
+              <div className="glass relative text-white flex flex-col items-center gap-4 md:h-[520px] md:w-[600px] sm:h-[443px] sm:w-[404px]  bg-white/10 border border-white/20 rounded-[35px]">
+                <img
+                  src="/assets/Group 36.png"
+                  alt="ellipses"
+                  className="absolute z-10"
+                ></img>
+                <div className=" text-center pt-8  sm:px-10 flex flex-col md:gap-8  font-sans justify-center items-center space-y-10">
+                  <h3 className=" font-bold text-2xl md:text-3xl md:px-14 px-8">
+                    Increase the global reach of your business!
+                  </h3>
+                  <p className="font-normal text-base md:text-lg">
+                    With our cutting-edge platform, Digigrove provide
+                    unparalleled digital marketing services to facilitate
+                    connections between brands and audiences that generate
+                    noteworthy results. We specialize in both direct response
+                    and branding strategies.
+                  </p>
+                </div>
               </div>
             </div>
-            </div>
-
           </div>
         </div>
 
@@ -113,7 +106,7 @@ const Affiliate = () => {
             className=" group-hover:text-black w-60 h-full text-xl bg-transparent border-t-[1px] border-b-[1px] border-l-[1px] border-white text-white font-semibold rounded-tl-full rounded-bl-full  transition-all duration-500 group-hover:bg-[#8cc540] group-hover:border-none   group-hover:rounded-full "
             id="btn1"
             onClick={() => {
-              navigate('/?scrollToContact=true')
+              navigate("/?scrollToContact=true");
             }}
           >
             Connect With Us!

2359c80e3aa820b5f52c2617962d7b899ffefe3b
Kshitij Dhama <kshitijdhama513@gmail.com>
2024-12-15 15:06:52 +0530

    rewrites

--- frontend/src/components/ECommerceMarketing.jsx
+++ frontend/src/components/ECommerceMarketing.jsx
@@ -25,17 +25,20 @@ const Marketing = () => {
           ></div>
 
           <div
-            className={`content h-full md:ml-0 sm:-ml-14 -ml-32  xl:pt-20 md:pt-12 pt-8  ${
-              !showDiv ? " w-full text-right" : "2xl:-ml-[30rem]  w-1/3"
+            className={`content h-full -ml-[40px] ${
+              !showDiv
+                ? "w-full text-right flex flex-col items-end"
+                : "2xl:-ml-[30rem]  w-1/3"
             }`}
           >
-            <h1 className="xl:text-[128px] lg:text-8xl sm:text-7xl text-6xl  tracking-[0.02em] font-thunder leading-1  whitespace-nowrap text-[#8CC540] font-semibold ">
+            <div>
+            <h1 className="max-[730px]:text-[42px] max-[821px]:text-[62px] max-[1039px]:text-[84px]  min-[1040px]:text-[128px] pb-[12px] tracking-[0.02em] font-thunder m whitespace-nowrap text-[#8CC540] font-semibold">
               {!showDiv ? "E-COMMERCE" : "INFLUENCER MARKETING"}
             </h1>
             <p
               className={`${
                 showDiv ? "text-white" : ""
-              } md:text-[18px] text-sm xl:text-2xl xl:pt-16 lg:pt-8 pt-4   font-medium text-[#232233]`}
+              } text-[18px] font-medium text-[#232233]`}
             >
               {!showDiv
                 ? `Transform your online store with Digigrove's tailored
@@ -44,14 +47,19 @@ const Marketing = () => {
               strategies.`
                 : `Leverage our global network of influencers to build authentic connections with your target audience. Digigrove ensures impactful collaborations that drive visibility and growth.`}
             </p>
-          </div>
-          <div className="inset-0 left-1/2 top-[15%] text-center p-4">
+            </div>
+
+
             <div
-              className={`pinner absolute ${
+              className={`pinner mt-[15%] max-[730px]:h-[120px] max-[821px]:h-[140px] max-[1039px]:h-[180px] min-[1040px]:h-[250px]
+                max-[730px]:w-[120px] max-[821px]:w-[140px] max-[1039px]:w-[180px] min-[1040px]:w-[250px]
+                ${
                 prepAnims ? (showDiv ? "right" : "left") : ""
               }`}
               onScrollCapture={toggleDiv}
             />
+          </div>
+          <div className="inset-0 left-1/2 top-[15%] text-center p-4">
             <div
               className={`clip absolute ${
                 prepAnims ? (showDiv ? "right" : "left") : ""
@@ -62,25 +70,37 @@ const Marketing = () => {
         </div>
       </div>
 
-      <div id="marketing-page" className={`main-section overflow-hidden right`}>
-        <div className="flex relative  pr-10">
+      <div
+        id="marketing-page"
+        className={`main-section overflow-hidden right`}
+      >
+                <div className="flex relative  pr-10 pb-8">
+
+
           <div
-            className={`content h-full   xl:pt-20 md:pt-12 pt-8 lg:pl-20 sm:pl-12 pl-6 }`}
+            className={`content h-full  pt-20 pl-20`}
           >
-            <h1 className="xl:text-[128px] lg:text-8xl sm:text-7xl text-6xl  tracking-[0.02em] font-thunder leading-1  2xl:whitespace-nowrap text-[#8CC540] font-semibold">
-              {"INFLUENCER MARKETING"}
+            <div>
+            <h1 className="leading-[124px] max-[730px]:text-[42px] max-[821px]:text-[62px] max-[1039px]:text-[84px]  min-[1040px]:text-[128px] pb-[12px] tracking-[0.02em] font-thunder m whitespace-nowrap text-[#8CC540] font-semibold
+            max-[730px]:leading-[42px] max-[821px]:leading-[62px] max-[1039px]:leading-[84px]  min-[1040px]:leading-[124px]
+            ">
+              INFLUENCER <br /> MARKETING
             </h1>
             <p
               className={`text-white md:text-[18px] text-sm xl:text-2xl xl:pt-16 lg:pt-8 pt-4   font-medium `}
             >
               {`Leverage our global network of influencers to build authentic connections with your target audience. Digigrove ensures impactful collaborations that drive visibility and growth.`}
             </p>
-          </div>
-          <div className="inset-0 left-1/2 top-[15%] text-center  w-1/2">
+            </div>
+
             <div
-              className={` pinner  absolute right`}
+                            className={`pinner mt-[15%] max-[730px]:h-[120px] max-[821px]:h-[140px] max-[1039px]:h-[180px] min-[1040px]:h-[250px]
+                              max-[730px]:w-[120px] max-[821px]:w-[140px] max-[1039px]:w-[180px] min-[1040px]:w-[250px]
+                              right`}
               onScrollCapture={toggleDiv}
             />
+          </div>
+          <div className="inset-0 left-1/2 top-[15%] text-center p-4">
             <div
               className={`clip absolute right`}
               onScrollCapture={toggleDiv}
--- frontend/src/components/Email.jsx
+++ frontend/src/components/Email.jsx
@@ -8,16 +8,16 @@ const Email = () => {
       id="email-marketing-container"
       className="lg:mt-[152px] mt-28 w-full flex  gap-6 xl:flex-wrap  items-center xl:items-start flex-col xl:flex-row pt-12 sm:py-0  "
     >
-      <div className="main flex flex-row">
-        <div className="left-side md:pl-[108px]  h-full w-full flex flex-col  sm:justify-evenly ">
+      <div className="main flex flex-col md:flex-row">
+        <div className="left-side h-auto md:pl-[108px]  h-full w-full flex flex-col  sm:justify-between ">
           <div
             id="text"
             className=" text-[#8cc540]  px-5 pb-6 sm:p-0 sm:pb-0   "
           >
-            <h3 className="md:tracking-[0.02em] leading-none text-center md:text-left  2xl:text-[110px] xl:text-[80px] md:text-[50px]   text-[40px] font-bold  font-thunder">
+            <h3 className="md:tracking-[0.02em] leading-none text-center md:text-left  2xl:text-[128px] xl:text-[80px] md:text-[50px]   text-[40px] font-bold  font-thunder">
               EMAIL MARKETING
             </h3>
-            <h3 className="md:tracking-[0.02em] leading-none text-center md:text-left  2xl:text-[110px] xl:text-[80px] md:text-[50px]  text-[40px]   font-bold  font-thunder">
+            <h3 className="md:tracking-[0.02em] leading-none text-center md:text-left  2xl:text-[128px] xl:text-[80px] md:text-[50px]  text-[40px]   font-bold  font-thunder">
               & LEAD GENERATION
             </h3>{" "}
             <h5 className="text-center md:text-left  text-black text-[18px] font-normal font-poppins">
@@ -27,18 +27,22 @@ const Email = () => {
             </h5>
           </div>
 
-          <div className="md:mt-[200px]">
+          <div className="hidden md:block md:mt-[200px]">
             <Button2 />
           </div>
         </div>
 
         <div className="right-side  h-full w-full flex justify-center items-center">
-          <div className="h-[220px] w-[220px] sm:h-[280px] sm:w-[346px] md:h-[390px] md:w-[456px] 2xl:h-[690px] 2xl:w-[756px] md:ml-4 sm:mb-[120px] md:mb-0">
+          <div className="flex flex-col items-center h-[220px] w-[220px] sm:h-[280px] sm:w-[346px] md:h-[390px] md:w-[456px] 2xl:h-[690px] 2xl:w-[756px] md:ml-4 sm:mb-[120px] md:mb-0">
             <img
               src="/assets/choosy/Frame32.png"
               alt="Responsive Image"
               className="h-full w-full object-contain"
             />
+
+          <div className="block md:hidden md:mt-[200px] pt-[20px]">
+            <Button2 />
+          </div>
           </div>
         </div>
       </div>
